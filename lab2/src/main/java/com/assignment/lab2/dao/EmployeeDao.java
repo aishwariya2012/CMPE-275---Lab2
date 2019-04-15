8ca88a7e70eb5858695b407dcbb9e8a2a4099031
divjyot3112 <divjyot.khanuja@sjsu.edu>
2019-04-14 19:06:54 -0700

    Updated EmployeeDao and EmployerDao

--- lab2/src/main/java/com/assignment/lab2/dao/EmployeeDao.java
+++ lab2/src/main/java/com/assignment/lab2/dao/EmployeeDao.java
@@ -1,9 +1,12 @@
 package com.assignment.lab2.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.assignment.lab2.entity.*;
 
 @Repository
 public interface EmployeeDao extends JpaRepository<Employee,Long> {
-
+	
+	@Query(value="Select CASE WHEN count(e)>0 THEN true ELSE false END FROM Employee e where e.employer.id = ?1")
+	boolean findEmpId(long id);
 }
--- lab2/src/main/java/com/assignment/lab2/dao/EmployerDao.java
+++ lab2/src/main/java/com/assignment/lab2/dao/EmployerDao.java
@@ -1,10 +1,14 @@
 package com.assignment.lab2.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.assignment.lab2.entity.*;
 
 @Repository
 public interface EmployerDao extends JpaRepository<EmployerEntity,Long> {
-
+	@Query(value="Select e from EmployerEntity e where e.name = ?1")
+	Optional<EmployerEntity> findByName(String name);
 }
